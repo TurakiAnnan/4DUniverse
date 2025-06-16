25db17cc270e6c32e6180130486c61f8090994c2
Turaki Annan <43704536+TurakiAnnan@users.noreply.github.com>
2025-06-16 03:30:28 +0100

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -1,47 +1,62 @@
-# simulator.py
 import numpy as np
+from scipy.spatial import KDTree
+import pandas as pd
+from collections import defaultdict
 
-# Constants
+# Simulation constants
 VOLUME_SIZE = 1000
 TIME_SPAN = 1000
-INTERSECTION_RADIUS = 2
-TIME_WINDOW = 2
-BLACKHOLE_CENTER = np.array([50, 50, 50])
-BLACKHOLE_RADIUS = 30
-BLACKHOLE_STRENGTH = 0.1
-GRAVITY_PULL_FACTOR = 0.02
+GRAVITY_STRENGTH = 0.01
+BLACK_HOLE_THRESHOLD = 50  # Number of collisions to form black hole
+BLACK_HOLE_RADIUS = 5
 
-def generate_streams(n):
-    streams = np.random.rand(n, 4)
+def generate_streams(n_streams):
+    streams = np.random.rand(n_streams, 4)
     streams[:, :3] *= VOLUME_SIZE
     streams[:, 3] *= TIME_SPAN
     return streams
 
-def apply_blackhole_gravity(streams):
-    for i in range(len(streams)):
-        dist = np.linalg.norm(streams[i, :3] - BLACKHOLE_CENTER)
-        if dist < BLACKHOLE_RADIUS:
-            direction = (BLACKHOLE_CENTER - streams[i, :3]) / (dist + 1e-5)
-            streams[i, :3] += BLACKHOLE_STRENGTH * direction
-    return streams
-
-def apply_local_gravity(streams, visible_matter):
-    for i in range(len(streams)):
-        dists = np.linalg.norm(visible_matter - streams[i, :3], axis=1)
-        close = visible_matter[dists < 10]
-        if len(close) > 0:
-            center = close.mean(axis=0)
-            direction = center - streams[i, :3]
-            streams[i, :3] += GRAVITY_PULL_FACTOR * direction
-    return streams
-
-def find_intersections(streams):
+def find_intersections(streams, radius=2, time_window=2):
     locked = []
-    for i in range(len(streams)):
-        for j in range(i+1, len(streams)):
-            if abs(streams[i, 3] - streams[j, 3]) < TIME_WINDOW:
-                dist = np.linalg.norm(streams[i, :3] - streams[j, :3])
-                if dist < INTERSECTION_RADIUS:
-                    midpoint = 0.5 * (streams[i, :3] + streams[j, :3])
-                    locked.append(midpoint)
-    return np.array(locked)
+    black_holes = []
+    density_map = defaultdict(int)
+    sorted_idx = np.argsort(streams[:, 3])
+    sorted_streams = streams[sorted_idx]
+
+    for i in range(len(sorted_streams)):
+        t_i = sorted_streams[i, 3]
+        j = i + 1
+        local_indices = []
+        local_points = []
+
+        while j < len(sorted_streams) and (sorted_streams[j, 3] - t_i) < time_window:
+            local_indices.append(j)
+            local_points.append(sorted_streams[j, :3])
+            j += 1
+
+        if local_points:
+            local_points = np.array(local_points)
+            tree = KDTree(local_points)
+            neighbors = tree.query_ball_point(sorted_streams[i, :3], radius)
+
+            for n in neighbors:
+                midpoint = 0.5 * (sorted_streams[i, :3] + local_points[n])
+                locked.append(midpoint)
+
+                # Track density
+                region = tuple((midpoint // BLACK_HOLE_RADIUS).astype(int))
+                density_map[region] += 1
+
+                # Black hole gravity boost
+                if density_map[region] > BLACK_HOLE_THRESHOLD:
+                    black_holes.append(midpoint)
+                    boost = GRAVITY_STRENGTH * 10
+                else:
+                    boost = GRAVITY_STRENGTH
+
+                # Gravitational pull
+                neighbor_idx = local_indices[n]
+                direction = midpoint - sorted_streams[neighbor_idx, :3]
+                sorted_streams[neighbor_idx, :3] += boost * direction
+
+    return np.array(locked), np.array(black_holes)
