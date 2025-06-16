13debffac47314596b9967bd2c31e5ee214929d5
Turaki Annan <43704536+TurakiAnnan@users.noreply.github.com>
2025-06-16 02:52:40 +0100

    Update exporter.py

--- exporter.py
+++ exporter.py
@@ -1,1 +1,10 @@
+# exporter.py
 
+import csv
+
+def export_locked_points(locked, filename="locked_matter.csv"):
+    with open(filename, "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["x", "y", "z"])
+        for pt in locked:
+            writer.writerow(pt)
